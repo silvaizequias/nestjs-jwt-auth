e242e61de3f0fce5f8887e517c08ec8463ddb22e
Izequias Silva <master@dedicado.digital>
2022-04-07 14:48:34 -0300

    ajustes de identação padrão com prettier e eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,23 +2,34 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   parserOptions: {
     project: 'tsconfig.json',
+    ecmaVersion: 2021,
     sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
   },
-  plugins: ['@typescript-eslint/eslint-plugin'],
+  plugins: ['@typescript-eslint/eslint-plugin', 'prettier'],
   extends: [
     'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
+    'eslint:recommended',
+    'prettier',
   ],
   root: true,
   env: {
-    node: true,
+    es6: true,
+    browser: true,
     jest: true,
+    node: true,
   },
   ignorePatterns: ['.eslintrc.js'],
   rules: {
     '@typescript-eslint/interface-name-prefix': 'off',
-    '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/explicit-function-return-type':
+      'off',
+    '@typescript-eslint/explicit-module-boundary-types':
+      'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    'prettier/prettier': 'error',
   },
 };
