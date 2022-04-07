272263883e1a13ae529956909f453d76e9b19933
Izequias Silva <master@dedicado.digital>
2022-04-07 15:40:45 -0300

    configurações funcionais de swagger e typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,17 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [TypeOrmModule.forRoot()],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.development.env',
+    }),
+    TypeOrmModule.forRoot(),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,32 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import {
+  SwaggerModule,
+  DocumentBuilder,
+} from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
+
+  const options = new DocumentBuilder()
+    .setTitle('API Sistema Dedicado')
+    .setDescription('API principal do Sistema Dedicado')
+    .setVersion('1.0')
+    .addTag('sd')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(
+    app,
+    options,
+  );
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
+  console.log(
+    `Application is running on: ${await app.getUrl()}`,
+  );
 }
 bootstrap();
